5de6bc45981a5117da322b470f2b9bb84d447b82
Mohankumar Swaminathan <smartmohi@gmail.com>
2018-08-25 10:57:44 +0530

    Update header.php

--- represent-map/header.php
+++ represent-map/header.php
@@ -1,11 +1,6 @@
 <?php
 include "./include/db.php";
 
-echo $db_host;
-echo $db_user;
-echo $db_pass;
-echo $db_name;
-
 // connect to db
 $msl = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 if (!$msl) {
